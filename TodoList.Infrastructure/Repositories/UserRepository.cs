32181d9b8b961017531440360692fb6f9fb467d4
codebyallan <allan.pereira.dev@proton.me>
2025-12-15 14:45:37 -0300

    feat(infrastructure): Implement User repository
    this commit Implements the complete UserRepository class, adhering to the IUserRepository contract.
    Includes the asynchronous methods: AddAsync, DeleteAsync, GetAllAsync, GetByIdAsync, and UpdateAsync.

---
+++ TodoList.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using TodoList.Domain.Entities;
+using TodoList.Domain.Interfaces;
+
+namespace TodoList.Infrastructure.Repositories;
+
+public class UserRepository(Persistence.ApplicationDbContext _applicationDbContext) : IUserRepository
+{
+    public async Task AddAsync(User user)
+    {
+        await _applicationDbContext.Users.AddAsync(user);
+    }
+    public Task DeleteAsync(User user)
+    {
+        _applicationDbContext.Users.Remove(user);
+        return Task.CompletedTask;
+    }
+    public async Task<IEnumerable<User>> GetAllAsync()
+    {
+        return await _applicationDbContext.Users.ToListAsync();
+    }
+    public async Task<User?> GetByIdAsync(int id)
+    {
+        return await _applicationDbContext.Users.FindAsync(id);
+    }
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _applicationDbContext.SaveChangesAsync();
+    }
+    public Task UpdateAsync(User user)
+    {
+        _applicationDbContext.Users.Update(user);
+        return Task.CompletedTask;
+    }
+}
