a407fa4f0ccd4a1414be1dd13008cf42865f3b2e
codebyallan <allan.pereira.dev@proton.me>
2025-12-15 14:33:07 -0300

    feat(infrastructure): Initial migration setup for the User entity
    this commit adds the 'Users' DbSet to the ApplicationDbContext. This is the required infrastructure setup to allow Entity Framework Core to track and manage the User entity in the database.

---
+++ TodoList.Infrastructure/Migrations/20251215172559_InitialUserEntity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoList.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialUserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- TodoList.Infrastructure/Persistence/ApplicationDbContext.cs
+++ TodoList.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -7,4 +7,6 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext() : base() { }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     public DbSet<TodoItem> TodoItems { get; set; }
+    public DbSet<User> Users { get; set; }
+
 }
